875f31d7a2da2de1408bdaa360a1e85181d037e7
cohen.andrew7@gmail.com <cohen.andrew7@gmail.com>
2020-07-19 00:33:23 -0600

    removed strings

--- Training.cpp
+++ Training.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -43,6 +43,64 @@ int main(void)
 			//cout << data[j] << endl;
 			//j++;
 		}
+
+		i++;
+
+	}
+
+	x_temp = x_init;
+
+	for (int k = 0; k < data[1]; k++)
+	{
+		x_new = (1 - 3 * data[0]) * x_temp;
+		x_temp = x_new;
+	}
+
+	cout << x_temp;
+
+	input.close();
+	output.close();
+	return 0;
+}
+
+*/
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(void)
+{
+
+	ifstream input;
+	ofstream output;
+	//string input_arr[6] = { "a","a","a","a","a","a" };
+	float data[2] = { 0,0 };
+	int i = 0;
+	int j = 0;
+	//stringstream ss;
+
+
+	int x_temp = 0;
+	int x_init = 1;
+	int x_new = 0;
+
+
+
+	input.open("params.dat");
+	output.open("output.dat");
+
+
+	//get data from params.dat
+	while (!input.eof())
+	{
+		input >> data[i];
+		cout << data[i] << endl;
+
 
 		i++;
 
