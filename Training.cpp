c02848e7a76acd5eeb05561a34be49a765f88d9c
cohen.andrew7@gmail.com <cohen.andrew7@gmail.com>
2020-07-29 01:01:56 -0600

    vectors and stuff <3

--- Training.cpp
+++ Training.cpp
@@ -1,75 +1,7 @@
-/*#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-using namespace std;
-
-int main(void)
-{
-
-	ifstream input;
-	ofstream output;
-	string input_arr[6] = { "a","a","a","a","a","a" };
-	int data[2] = { 0,0 };
-	int i = 0;
-	int j = 0;
-	stringstream ss;
-
-
-	int x_temp = 0;
-	int x_init = 1;
-	int x_new = 0;
-
-
-
-	input.open("params.dat");
-	output.open("output.dat");
-
-
-	//get data from params.dat
-	while (input_arr[i] != "")
-	{
-		input >> input_arr[i];
-		cout << input_arr[i] << endl;
-		if ((i + 1) % 3 == 0)
-		{
-			ss << input_arr[i] << ' ';
-
-			ss >> data[j];
-			j++;
-			cout << data[j - 1] << endl;
-
-			//data[j] = stof(input_arr[i]);
-			//cout << data[j] << endl;
-			//j++;
-		}
-
-		i++;
-
-	}
-
-	x_temp = x_init;
-
-	for (int k = 0; k < data[1]; k++)
-	{
-		x_new = (1 - 3 * data[0]) * x_temp;
-		x_temp = x_new;
-	}
-
-	cout << x_temp;
-
-	input.close();
-	output.close();
-	return 0;
-}
-
-*/
-
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <sstream>
 using namespace std;
 
@@ -78,16 +10,16 @@ int main(void)
 
 	ifstream input;
 	ofstream output;
-	//string input_arr[6] = { "a","a","a","a","a","a" };
 	float data[2] = { 0,0 };
 	int i = 0;
 	int j = 0;
-	//stringstream ss;
-	//
 
 	int x_temp = 0;
 	int x_init = 1;
 	int x_new = 0;
+
+	vector<float> x_vector;
+	vector<float> time_vector;
 
 
 
@@ -99,7 +31,7 @@ int main(void)
 	while (!input.eof())
 	{
 		input >> data[i];
-		cout << data[i] << endl;
+		//cout << data[i] << endl;
 
 
 		i++;
@@ -111,10 +43,24 @@ int main(void)
 	for (int k = 0; k < data[1]; k++)
 	{
 		x_new = (1 - 3 * data[0]) * x_temp;
+		x_vector.push_back(x_new);
+		time_vector.push_back((k + 1) * data[0]);
 		x_temp = x_new;
 	}
 
-	cout << x_temp;
+	cout << x_temp << endl;
+	
+	for (auto z = x_vector.begin(); z != x_vector.end(); ++z)
+	{
+		cout << *z << ' ';
+	}
+
+	for (auto z = time_vector.begin(); z != time_vector.end(); ++z)
+	{
+		cout << *z << ' ';
+	}
+
+
 
 	input.close();
 	output.close();
